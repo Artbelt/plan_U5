e06ba6a1ef05c0499d0613472f8b1dcc768f6e15
Artem Belchikov <artbelt.pooh@gmail.com>
2025-10-28 12:41:34 +0200

    справлено название колонки filter вместо filter_name

--- NP_roll_plan.php
+++ NP_roll_plan.php
@@ -27,7 +27,7 @@ if (!$hasPlanReadyCol) {
 /* Бухты из cut_plans с complexity */
 $stmt = $pdo->prepare("SELECT cp.bale_id, cp.filter, cp.height, cp.width, sfs.build_complexity
                        FROM cut_plans cp
-                       LEFT JOIN salon_filter_structure sfs ON cp.filter = sfs.filter_name
+                       LEFT JOIN salon_filter_structure sfs ON cp.filter = sfs.filter
                        WHERE cp.order_number = ?
                        ORDER BY cp.bale_id, cp.strip_no");
 $stmt->execute([$order]);
